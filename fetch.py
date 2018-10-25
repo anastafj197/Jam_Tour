80dbbba0de52b80f7b98d78d9e20959c515c9873
anastafj197 <frankie.anastasia@gmail.com>
2018-10-24 20:37:11 -0400

    create fetching file
    Contains:  
    Bands list to choose from  
    Url appender

---
+++ fetch.py
@@ -0,0 +1,35 @@
+# Fetching data from jamBase.com 
+
+base_url = "https://www.jambase.com/band/"
+
+# band names to match jamBases urls 
+bands = [
+		"dark-star-orchestra",
+		"dead-company",
+		"dopapod",
+		"joe-russos-almost-dead",
+		"lotus",
+		"lettus",
+		"max-creek",
+		"moe",
+		"phil-lesh-friends",
+		"phish",
+		"pigeons-playing-ping-pong",
+		"tedeschi-trucks-band",
+		"the-disco-biscuits",
+		"the-motet",
+		"the-string-cheese-incident",
+		"the-werks",
+		"trey-anastasio-band",
+		"turkuaz",
+		"twiddle",
+		"umphreys-mcgee",
+		"widespread-panic",
+		]
+
+# check if urls are being appended properly
+for band in bands:
+	url = base_url + band
+	print(url)
+	url = base_url
+ 
