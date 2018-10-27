980196bdef3279ec55f34765063cb842c5ffd297
anastafj197 <frankie.anastasia@gmail.com>
2018-10-27 19:46:51 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 #html = urlopen("https://www.jambase.com/band/moe")
 #bsObj = BeautifulSoup(html, "lxml")
 
-class fetch:
+def fetch():
 
 	base_url = "https://www.jambase.com/band/"
 
@@ -58,44 +58,44 @@ class fetch:
 	print(urls)
 	print()
 
-class State(object):
+class State:
 	# May add on region in constructor 
 	def _init_(self, name, abv, selected):
 		self.name = name 
 		self. abv = abv 
 		self.selected = False 
 
-	state_dict = {}
+state_dict = {}
 
-	states = [
-			"Alabama", "Alaska", "Arizona", "Arkansas", 
-			"California", "Colorado", "Connecticut", 
-			"Delaware", "District of Columbia", "Florida", 
-			"Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
-			"Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
-			"Maryland", "Massachusetts", "Michigan", "Minnesota",
-			"Mississippi", "Missouri", "Montana", "Nebraska",
-			"Nevada", "New Hampshire", "New Jersey", "New Mexico",
-			"New York", "North Carolina", "North Dakota", "Ohio",
-			"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
-			"South Carolina", "South Dakota", "Tennessee",
-			"Texas", "Utah", "Vermont", "Virginia", "Washington",
-			"West Virginia", "Wisconsin", "Wyoming"
-			 ]
+states = [
+		"Alabama", "Alaska", "Arizona", "Arkansas", 
+		"California", "Colorado", "Connecticut", 
+		"Delaware", "District of Columbia", "Florida", 
+		"Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
+		"Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+		"Maryland", "Massachusetts", "Michigan", "Minnesota",
+		"Mississippi", "Missouri", "Montana", "Nebraska",
+		"Nevada", "New Hampshire", "New Jersey", "New Mexico",
+		"New York", "North Carolina", "North Dakota", "Ohio",
+		"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+		"South Carolina", "South Dakota", "Tennessee",
+		"Texas", "Utah", "Vermont", "Virginia", "Washington",
+		"West Virginia", "Wisconsin", "Wyoming"
+		 ]
 
-	abvs =  [
-			"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC",
-			"FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
-			"LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
-			"NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
-			"OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
-			"VT", "VA", "WA", "WV", "WI", "WY"
-			]
+abvs =  [
+		"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC",
+		"FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
+		"LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
+		"NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH",
+		"OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+		"VT", "VA", "WA", "WV", "WI", "WY"
+		]
 
-	# attempting to make a new state obj for each state 
-	ix = 0 
-	for state in states:		
-		state_dict[states[ix]] = State(states[ix], abvs[ix], False)
-		index += 1 
+# attempting to make a new state obj for each state 
+ix = 0 
+for state in states:		
+	state_dict[states[ix]] = State(states[ix], abvs[ix], False)
+	index += 1 
 
-	print(state_dict)
+print(state_dict)
