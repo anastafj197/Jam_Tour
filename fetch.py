1726607d2033a63db39103f1c5bcc9b44469c5b4
anastafj197 <frankie.anastasia@gmail.com>
2018-10-27 17:14:54 -0400

    Began state map
    Making a state object with four qualities

--- fetch.py
+++ fetch.py
@@ -1,47 +1,76 @@
 # Fetching data from jamBase.com 
+class fetch:
 
-base_url = "https://www.jambase.com/band/"
+	base_url = "https://www.jambase.com/band/"
 
-# band names to match jamBases urls 
-bands = [
-		"dark-star-orchestra",
-		"dead-company",
-		"dopapod",
-		"joe-russos-almost-dead",
-		"lotus",
-		"lettus",
-		"max-creek",
-		"moe",
-		"phil-lesh-friends",
-		"phish",
-		"pigeons-playing-ping-pong",
-		"tedeschi-trucks-band",
-		"the-disco-biscuits",
-		"the-motet",
-		"the-string-cheese-incident",
-		"the-werks",
-		"trey-anastasio-band",
-		"turkuaz",
-		"twiddle",
-		"umphreys-mcgee",
-		"widespread-panic",
-		]
+	# band names to match jamBases urls 
+	bands = [
+			"dark-star-orchestra",
+			"dead-company",
+			"dopapod",
+			"joe-russos-almost-dead",
+			"lotus",
+			"lettus",
+			"max-creek",
+			"moe",
+			"phil-lesh-friends",
+			"phish",
+			"pigeons-playing-ping-pong",
+			"tedeschi-trucks-band",
+			"the-disco-biscuits",
+			"the-motet",
+			"the-string-cheese-incident",
+			"the-werks",
+			"trey-anastasio-band",
+			"turkuaz",
+			"twiddle",
+			"umphreys-mcgee",
+			"widespread-panic",
+			]
 
-# festivals 
-festi = [
-		"camp-bisco",
-		"summer-camp",
-		"lockn",
-		"high-siera",
-		]
+	# festivals 
+	festi = [
+			"camp-bisco",
+			"summer-camp",
+			"lockn",
+			"high-siera",
+			]
 
-# urls to visit 
-urls = []
+	# urls to visit 
+	urls = []
 
-# check if urls are being appended properly
-for band in bands:
-	url = base_url + band
-	urls.append(url)
-	url = base_url
+	# check if urls are being appended properly
+	for band in bands:
+		url = base_url + band
+		urls.append(url)
+		url = base_url
  
-print(urls)
+	print(urls)
+
+class state:
+	def _init_(name, abv, region, selected):
+
+	states = [
+			"Alabama", "Alaska", "Arizona", "Arkansas", 
+			"California", "Colorado", "Connecticut", 
+			"Delaware", "District of Columbia", "Florida", 
+			"Georgia", "Hawaii", "Idaho", "Illinois", "Indiana",
+			"Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+			"Maryland", "Massachusetts", "Michigan", "Minnesota",
+			"Mississippi", "Missouri", "Montana", "Nebraska",
+			"Nevada", "New Hampshire", "New Jersey", "New Mexico",
+			"New York", "North Carolina", "North Dakota", "Ohio",
+			"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+			"South Carolina", "South Dakota", "Tennessee",
+			"Texas", "Utah", "Vermont", "Virginia", "Washington",
+			"West Virginia", "Wisconsin", "Wyoming"
+			 ]
+			 
+	abvs =  [
+			"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE",
+			"FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY",
+			"LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT",
+			"NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH",
+			"OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+			"VA", "VT", "WA", "WI", "WV", "WY"
+			]
