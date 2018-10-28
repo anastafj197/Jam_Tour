7473a9c75299851a9470e387e76bd7bb0e910d34
anastafj197 <frankie.anastasia@gmail.com>
2018-10-28 18:45:42 -0400

    Added btn_dict
    created a btn for each state

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import fetch
 
+from fetch import state_dict
 from tkinter import *
 
 master = Tk()
@@ -8,9 +9,22 @@ def highlight():
 	exit()
 
 # Accepts Tk object, btn name, and function 
-button = Button(master, text = "State", command = highlight)
+#button = Button(master, text = "State", command = highlight)
 
 # Need to pack every button made 
-button.pack()
+#button.pack()
+
+btn_dict = {}
+
+# Insert btns into a dictionary of buttons with 
+# corresponding state names attached 
+def fill_btn_dict():
+
+	for state in state_dict: 
+		btn_dict[state] = Button(master, text = state, command = highlight)
+		btn_dict[state].pack()
+		print(state)
+
+fill_btn_dict()
 
 mainloop()
