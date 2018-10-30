3429c471d708de412d4b2884d971591fe21dea75
anastafj197 <frankie.anastasia@gmail.com>
2018-10-30 00:18:01 -0400

    Add To dos

--- check.py
+++ check.py
@@ -7,3 +7,14 @@ from fetch import state_dict
 
 my_bands = []
 
+# Will run after the choice is made on gui 
+# For which states are selected 
+
+# To do:
+# Button in gui.py to verify choices return the selected bands 
+
+# Festivals
+# associate:
+# festival with state its held in 
+# festival with most commonly attended bands
+
