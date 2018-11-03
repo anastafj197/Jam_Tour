a23a5a8e7edde39c45cbe21055a6c83c07b5979b
anastafj197 <frankie.anastasia@gmail.com>
2018-11-02 22:03:06 -0400

    Add selected states to an array

--- check.py
+++ check.py
@@ -1,11 +1,21 @@
 import gui 
 import fetch
+import pprint 
 
 from fetch import state_dict
 
 # extract data from selected states 
 
-my_bands = []
+my_states = []
+
+for state in state_dict:
+	if state_dict[state].selected == True:
+		print(state)
+
+	#if state.selected == true: 
+		#my_bands.append(state)
+
+#pprint.pprint(state_dict)
 
 # Will run after the choice is made on gui 
 # For which states are selected 
@@ -17,4 +27,3 @@ my_bands = []
 # associate:
 # festival with state its held in 
 # festival with most commonly attended bands
-
